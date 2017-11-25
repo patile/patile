1adb21cebd78ed1a05f03207ad3705a5c372b726
yagmursato <yagmur_sato@windowslive.com>
2017-11-25 13:03:35 +0300

    index.html ve base.html yapıldı

--- Patile/Patile/settings.py
+++ Patile/Patile/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -112,3 +113,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = "/static/"
+
