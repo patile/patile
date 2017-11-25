140685a1c146283c73f6add25b3ef991b3c3caf2
utkucanbykl <utkucanbykl@gmail.com>
2017-11-25 13:52:35 +0300

    Patient Model Eklendi

--- Patile/patient/models.py
+++ Patile/patient/models.py
@@ -1,1 +1,17 @@
 # Create your models here.
+from django.db import models
+
+
+class Patient(models.Model):
+    STATUS_TYPE = ('AVAILABLE', 'Available'), ('TREATMENT', 'In Treatment'), ("DONE", "Treatment Done")
+    name = models.CharField(max_length=100, default="Jane Doe")
+    status = models.CharField(max_length=30, choices=STATUS_TYPE, default="AVAILABLE")
+    telegram_id = models.CharField(max_length=140)
+    description = models.TextField(max_length=140, blank=True, null=True)
+    before = models.ImageField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField()  # auto now add nasil calisiyor
+    type = models.CharField(max_length=40, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
