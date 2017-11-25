22aaed398a5572656a8c901236fade6c537eeeda
tahademir95 <tahadmr.1995@gmail.com>
2017-11-25 19:53:42 +0300

    Veteriner admin paneli düzenlendi.

--- Patile/veterinary/urls.py
+++ Patile/veterinary/urls.py
@@ -7,4 +7,7 @@ app_name = 'veterinary'
 urlpatterns = [
     url(r'^accept', views.accept_patient, name='accept'),
     url(r'^$', views.login,name='login'),
+    url(r'^illl/', views.ill, name='ill'),
+    url(r'^under_treatment/', views.under_treatment, name='under_treatment'),
+    url(r'^success/', views.success, name='success'),
 ]
--- Patile/veterinary/views.py
+++ Patile/veterinary/views.py
@@ -3,6 +3,12 @@ from django.shortcuts import render
 
 def login(request):
     return render(request, 'vet-home.html')
+def ill(request):
+    return render(request, 'ill_animal-detail.html')
+def under_treatment(request):
+    return render(request, 'under_treatment-detail.html')
+def success(request):
+    return render(request, 'success-detail.html')
 
 def accept_patient(request):
     return render(request, "")
