35e784c4c745b371c71290978a7360e4ba6f462b
yagmursato <yagmur_sato@windowslive.com>
2017-11-25 16:06:49 +0300

    Vets ve pets sayfaları yapıldı

--- Patile/home/urls.py
+++ Patile/home/urls.py
@@ -2,7 +2,13 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from home import views
+app_name = 'home'
 
 urlpatterns = [
-    url(r'^$', views.index,name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^pets/$', views.pets, name='pets'),
+    url(r'^singlePet/$', views.singlePet, name='singlePet'),
+    url(r'^vets/$', views.vets, name='vets'),
+    url(r'^singleVet/$', views.singleVet, name='singleVet'),
+
 ]
--- Patile/home/views.py
+++ Patile/home/views.py
@@ -3,3 +3,16 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def pets(request):
+    return render(request, 'pets.html')
+
+def vets(request):
+    return render(request, 'vets.html')
+
+def singlePet(request):
+    return render(request, 'singlePet.html')
+
+def singleVet(request):
+    return render(request, 'singleVet.html')
+
