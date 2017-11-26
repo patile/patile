a330056a36ab3d5c2e9db0e96e8abd092af4eb68
utkucanbykl <utkucanbykl@gmail.com>
2017-11-26 05:23:40 +0300

    Vet ajaxları yapıldı

--- Patile/veterinary/urls.py
+++ Patile/veterinary/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     url(r'^illl/', views.ill, name='ill'),
     url(r'^under_treatment/', views.under_treatment, name='under_treatment'),
     url(r'^success/', views.success, name='success'),
+    url(r'^vetpet/', views.vet_patient, name='all'),
+    url(r'^vetcount/', views.vet_count, name='count'),
+
 ]
--- Patile/veterinary/views.py
+++ Patile/veterinary/views.py
@@ -5,17 +5,24 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 
 from patient.models import Patient
-from veterinary.models import Veterinary_Patient
+from veterinary.models import Veterinary_Patient, Veterinary
 
 
 def login(request):
     return render(request, 'vet-home.html')
+
+
 def ill(request):
     return render(request, 'ill_animal-detail.html')
+
+
 def under_treatment(request):
     return render(request, 'under_treatment-detail.html')
+
+
 def success(request):
     return render(request, 'success-detail.html')
+
 
 def accept_patient(request):
     if request.method == "POST":
@@ -42,10 +49,22 @@ def vet_patient(request):
     results = []
     if request.method == "POST":
         vet = request.user
-        vet_pet = Veterinary_Patient.objects.filter(veterinary=vet)
+        vet_pet = Veterinary_Patient.objects.filter(veterinary=Veterinary.objects.get())
         response_json = {}
         for pet in vet_pet:
             response_json["name"] = pet.patient.name
             results.append(response_json)
         data = json.dumps(results)
+        return HttpResponse(data)
+
+
+def vet_count(request):
+    if request.method == "POST":
+        vet = Veterinary.objects.get(id=1)
+        active_pet = Veterinary_Patient.objects.filter(veterinary=vet, patient__status="AVAILABLE").count()
+        close_pet = Veterinary_Patient.objects.filter(patient__status="DONE", veterinary=vet).count()
+        response_json = {}
+        response_json["active_count"] = str(active_pet)
+        response_json["close_count"] = str(close_pet)
+        data = json.dumps(response_json)
         return HttpResponse(data)
