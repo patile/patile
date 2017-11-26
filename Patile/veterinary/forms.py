79be60d99bd82d92525f0fba78a4f4386f5878d2
utkucanbykl <utkucanbykl@gmail.com>
2017-11-26 07:47:00 +0300

    Placeholder eklendi

--- Patile/veterinary/forms.py
+++ Patile/veterinary/forms.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    first_name = forms.CharField(max_length=30)
+    last_name = forms.CharField(max_length=30)
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
 
     class Meta:
@@ -14,9 +14,11 @@ class SignUpForm(UserCreationForm):
 
 
 class LoginForm(forms.ModelForm):
-    username = forms.CharField(label="Kullanıcı Adı", help_text="")
+    username = forms.CharField(label="Kullanıcı Adı", help_text="",
+                               widget=forms.TextInput(attrs={'placeholder': 'Kullanıcı Adı'}))
+
     password = forms.CharField(label='Parola:',
-                               widget=forms.PasswordInput)
+                               widget=forms.PasswordInput(attrs={'placeholder': 'Parola'}))
 
     class Meta:
         model = User
